2f4b668ce99fd72f2d15fc91bdb58fc64061170d
Stoyan I. Stoyanov <si.stoyanov@tinqin.com>
2025-11-18 12:32:56 +0200

    add login onwer or syndic logic

--- mock/app.mock.ts
+++ mock/app.mock.ts
@@ -1,6 +1,8 @@
 import { defineMock } from "vite-plugin-mock-dev-server";
 
-import qr from "./auth/login-email_POST.json";
+import qr_syndic from "./auth/login-email_syndic_POST.json";
+import qr_coowner from "./auth/login-email_coowner_POST.json";
+// import qr from "./auth/login-email_POST.json";
 import meetings from "./assemblies/_GET.json";
 import stats from "./assemblies/stats_GET.json";
 
@@ -8,7 +10,13 @@ export default defineMock([
   {
     url: "/api/auth/login-email",
     method: "POST",
-    body: qr,
+    body: (req) => {
+      const { email } = req.body;
+
+      const qr = email === "owner@prop.bg" ? qr_coowner : qr_syndic;
+
+      return qr;
+    },
   },
   {
     url: "/api/assemblies",
